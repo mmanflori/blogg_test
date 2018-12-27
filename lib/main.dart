f07d8b2b8f31289719db467aeb37b18df3092cad
Florian Brielmaier <mmanflori@gmail.com>
2018-12-27 16:21:30 +0100

    textänderungen

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class flutter_lernen extends StatelessWidget {
               Container(
                 width: 53.0,
                 height: 53.0,
+                margin: EdgeInsets.symmetric(vertical: 2.0),
                 child: Icon(
                   Icons.person,
                   size: 53.0,
@@ -35,12 +36,28 @@ class flutter_lernen extends StatelessWidget {
                   Container(
                     height: 27.0,
                     width: 158.0,
-                    color: Colors.redAccent,
+                    margin: EdgeInsets.symmetric(vertical: 2.0),
+                    color: Colors.grey[100],
+                    child: Center(
+                      child: Text(
+                        "Verkauf",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontWeight: FontWeight.bold,letterSpacing: 10.0),
+                      ),
+                    ),
                   ),
                   Container(
                     height: 27.0,
                     width: 158.0,
-                    color: Colors.deepOrange,
+                    margin: EdgeInsets.symmetric(vertical: 2.0),
+                    color: Colors.grey[200],
+                    child: Center(
+                      child: Text(
+                        "Hans Mustermann",
+                        textAlign:TextAlign.center,
+                        style:TextStyle(fontWeight:FontWeight.bold,letterSpacing:2.0),
+                      ),
+                    ),
                   )
                 ],
               ),
@@ -52,16 +69,28 @@ class flutter_lernen extends StatelessWidget {
               Container(
                 width: 132.0,
                 height: 42.0,
-                color: Colors.indigo,
+                margin: EdgeInsets.symmetric(horizontal: 2.0),
+                color: Colors.grey[200],
+                child: Center(
+                  child: Text(
+                    "099 837 14 99",
+                    textAlign:TextAlign.center,
+                    style:TextStyle(
+                      fontWeight:FontWeight.bold,letterSpacing: 2.0,
+                    )
+                  ),
+                ),
               ),
               Container(
                 width: 53.0,
                 height: 42.0,
+                margin: EdgeInsets.symmetric(horizontal: 2.0),
                 color: Colors.red,
               ),
               Container(
                 width: 53.0,
                 height: 42.0,
+                margin: EdgeInsets.symmetric(horizontal: 2.0),
                 color: Colors.yellow,
               ),
             ],
